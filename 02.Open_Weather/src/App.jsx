ace52d85055234ff7ee10aed435c7aa6d3f3a6a3
jesualujan <lujan.oficial@gmail.com>
2024-11-28 21:05:30 -0600

    🎉 Creación del proyecto

---
+++ 02.Open_Weather/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Open weather</h1>
+    </>
+  )
+}
+
+export default App
---
+++ Readme.md
@@ -0,0 +1,3 @@
+> [!TIP]
+> Api que usaremos para el ejemplo 02.Open_Weather: 
+> `https://openweathermap.org/api`
